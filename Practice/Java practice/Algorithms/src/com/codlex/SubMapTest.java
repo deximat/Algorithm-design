240cb7b97c59830f7e184013740243eb01532e77
Dejan Pekter <deximat@gmail.com>
2016-04-13 15:25:26 +0200

    fsdfds

--- Practice/Java practice/Algorithms/src/com/codlex/SubMapTest.java
+++ Practice/Java practice/Algorithms/src/com/codlex/SubMapTest.java
@@ -1,23 +1,36 @@
 package com.codlex;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
 
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class SubMapTest {
 	
 	public static void main(String[] args) {
-		TreeMap<Integer, Integer> longsMap = new TreeMap<>();
-		Random random = new Random();
-		for (int i = 0; i < 1000000; i++) {
-			longsMap.put(random.nextInt(), random.nextInt());
-		}
 		
-		Stopwatch stop = new Stopwatch();
-		for (Integer number : longsMap.subMap(1, 1000000).values()) {
-			System.out.println(number);
-		}		
-		System.out.println("Time: " + stop.elapsedTime());
+		Map<K, V> map = new HashMap<>();
+		
+		map.put(key, value)
+		
+		StringBuilder builder = new StringBuilder();
+		builder.
+		System.out.println(Integer.toBinaryString(Integer.parseInt("0", 16)));
+//		
+//		TreeMap<Integer, Integer> longsMap = new TreeMap<>();
+//		Random random = new Random();
+//		for (int i = 0; i < 1000000; i++) {
+//			longsMap.put(random.nextInt(), random.nextInt());
+//		}
+//		
+//		Stopwatch stop = new Stopwatch();
+//		for (Integer number : longsMap.subMap(1, 1000000).values()) {
+//			System.out.println(number);
+//		}		
+//		System.out.println("Time: " + stop.elapsedTime());
 	}
 }
